60f4a23fd17b2eacf4492fbd8f7bfd95e69b32d6
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-09 19:01:14 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,11 +5,6 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
-  get '/songs/:slug' do
-    @song = Song.find_by_slug(params[:slug])
-    erb :'/songs/show'
-  end
-
   get '/songs/new' do
     erb :'/songs/new'
   end
@@ -32,4 +27,9 @@ class SongsController < ApplicationController
     redirect '/songs/#{@song.slug}'
   end
 
+  get '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    erb :'/songs/show'
+  end
+  
 end
