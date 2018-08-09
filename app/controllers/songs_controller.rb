10b57cf99ecd6f178852ade3086c1180bbc9902a
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-09 19:48:51 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,8 +12,8 @@ class SongsController < ApplicationController
   post '/songs/new' do
     @song = Song.new(name: params[:name])
     binding.pry
-    if params[:artist_check]
-      
+    # if params[:artist_check]
+
     if !Artist.find_by(name: params[:artist])
       @artist = Artist.new(name: params[:artist])
     else
