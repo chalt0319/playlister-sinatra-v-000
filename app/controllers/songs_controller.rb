30322980c2323ae85c0f802bbfd8aee914cd1e71
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-09 19:23:04 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -37,7 +37,7 @@ class SongsController < ApplicationController
     erb :'songs/edit'
   end
 
-  patch '/songs/:slug' do
+  post '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     if @song.name != params[:name]
       @song.name = params[:name]
